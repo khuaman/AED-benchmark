23ab22b80fba4145cb4cca2b26ac626ec8cdf35f
khuaman <kevin.huaman@utec.edu.pe>
2023-08-20 09:10:57 -0500

    refactor: change typos and add comments

--- main.cpp
+++ main.cpp
@@ -8,32 +8,33 @@
 using namespace std;
 
 int main(){
-    srand(static_cast<unsigned int>(time(nullptr)));
+    std::random_device rd;
+    std::mt19937_64 generator(rd());
+    std::uniform_int_distribution<long long int> distribution(1, 9223372036854775807);
+
 
     // 10 elementos
-    const int size = 1000000; // Tamaño del array
-    long long int* array = new long long int[size];
+    const long long int size = 100000000; // Size of array
+    auto array = new long long int[size];
 
 
     for (auto i = 0; i < size; i++) {
-        array[i] = (rand() % 9223372036854775807) + 1;
+        array[i] = distribution(generator);
     }
 
-    // auto N = sizeof(*array) / sizeof(array[0]);
+   //auto N = sizeof(array)/sizeof(array[0]);  Not necessary because obtain the same number of size
 
     auto start = std::chrono::high_resolution_clock::now();
-
-    // FUNCION
 
     // Function call
     heapSort(array, size);
 
-    auto final = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(final - start);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
     delete[] array;
 
-    std::cout << "Tiempo de ejecucion: " << duration.count() << " milisegundos" << std::endl;
+    std::cout << "Execution time: " << duration.count() << " milliseconds" << std::endl;
 
     return 0;
 }
