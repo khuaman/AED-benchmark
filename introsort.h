e643d7bfb2c4e2ba7863651cb35f109cf0ea7e81
Melisa Rivera <melisa.rivera@utec.edu.pe>
2023-08-24 05:27:18 -0500

    mejoras en el introsort y nuevos tiempos

--- introsort.h
+++ introsort.h
@@ -1,88 +1,97 @@
 #ifndef AED_BENCHMARK_INTROSORT_H
 #define AED_BENCHMARK_INTROSORT_H
+
 #include <iostream>
-#include <cstdlib>
 #include <cmath>
-#include <algorithm>
+
+using namespace std;
+
+template<typename T>
+void insertionsort(T* array, long long int size) {
+    for (int i=1; i<size; i++){
+        int j=i;
+        T element = array[i];
+        while(j!=0 and array[j-1] > element){
+            array[j] = array[j-1];
+            j--;
+        }
+        array[j] = element;
+    }
+}
 
 
-// Función para realizar la operación Heapify en Heapsort
-void introsort_introsort_heapify(long long int* arr, long long int n, long long int i) {
+template <typename T>
+int QuickOrdenacion(T* array, int inicio, int final){
+    int i = inicio;
+    int puntero = i-1;
+    T pivote = array[final];
+    while(i<=final) {
+        if (array[i] <= pivote) {
+            puntero++;
+            if (i > puntero) {
+                T temporal = array[puntero];
+                array[puntero] = array[i];
+                array[i] = temporal;
+            } else if (i == puntero) {
+                i++;
+            }
+        } else {
+            i++;
+        }
+    }
+    return puntero;
+}
+
+template <typename T>
+void heapify(T* arr, long long int N, long long int i) {
     auto largest = i;
     auto left = 2 * i + 1;
     auto right = 2 * i + 2;
 
-    if (left < n && arr[left] > arr[largest])
+    if (left < N && arr[left] > arr[largest])
         largest = left;
 
-    if (right < n && arr[right] > arr[largest])
+    if (right < N && arr[right] > arr[largest])
         largest = right;
 
     if (largest != i) {
-        std::swap(arr[i], arr[largest]);
-        introsort_introsort_heapify(arr, n, largest);
+        swap(arr[i], arr[largest]);
+        heapify(arr, N, largest);
     }
 }
 
-// Función de ordenamiento Heapsort
-void introsort_heapsort(long long int arr[], long long int n) {
-    for (auto i = n / 2 - 1; i >= 0; i--)
-        introsort_introsort_heapify(arr, n, i);
+template <typename T>
+void heapSort(T* arr, long long int N) {
+    for (auto i = N / 2 - 1; i >= 0; i--)
+        heapify(arr, N, i);
 
-    for (auto i = n - 1; i > 0; i--) {
-        std::swap(arr[0], arr[i]);
-        introsort_introsort_heapify(arr, i, 0);
+    for (auto i = N - 1; i > 0; i--) {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
     }
 }
 
-// Función de ordenamiento por inserción
-void introsort_insertionSort(long long* arr, long long n) {
-    for (auto i = 1; i < n; i++) {
-        auto key = arr[i];
-        auto j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = key;
-    }
-}
-
-// Función de partición para el algoritmo Quicksort
-int partition(long long int* arr, long long int low, long long int high) {
-    auto pivot = arr[high];
-    auto i = (low - 1);
-
-    for (auto j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            std::swap(arr[i], arr[j]);
-        }
-    }
-    std::swap(arr[i + 1], arr[high]);
-    return (i + 1);
-}
-
-// Función de ordenamiento Quicksort
-void introsort_quicksort(long long int* arr, long long int low, long long int high, long long int depthLimit) {
-    while (high - low > 16) {
-        if (depthLimit == 0) {
-            introsort_heapsort(arr, high - low + 1);
-            return;
-        }
-        depthLimit--;
-
-        int pivotIndex = partition(arr, low, high);
-        introsort_quicksort(arr, low, pivotIndex - 1, depthLimit);
-        low = pivotIndex + 1;
+template<typename T>
+void introsort(T* array, int inicio, int final, int depth_limit) {
+    if (final - inicio <= 8) {  // Umbral para usar InsertionSort
+        insertionsort(array + inicio, final - inicio + 1);
+        return;
     }
 
-    introsort_insertionSort(arr + low, high - low + 1);
+    if (depth_limit == 0) {  // Umbral de recursión, usar HeapSort
+        heapSort(array + inicio, final - inicio + 1);
+        return;
+    }
+
+    int separador = QuickOrdenacion(array, inicio, final);
+    introsort(array, inicio, separador - 1, depth_limit - 1);
+    introsort(array, separador + 1, final, depth_limit - 1);
 }
 
-// Función de ordenamiento Introsort
-void introsort(long long int* arr, long long int n) {
-    auto depthLimit = 2 * static_cast<long long int>(std::log2(n));
-    introsort_quicksort(arr, 0, n - 1, depthLimit);
+template<typename T>
+void introsort(T* array, int size) {
+    int depth_limit = 2 * log(size);  // Límite de recursión, puedes ajustarlo
+    introsort(array, 0, size - 1, depth_limit);
 }
+
 #endif
