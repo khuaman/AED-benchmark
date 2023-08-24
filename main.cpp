ef8b7fcaf3379b7542582f05b5c2f7ffc9666a17
khuaman <kevin.huaman@utec.edu.pe>
2023-08-24 02:01:05 -0500

    fead: version estable con todo junto

--- main.cpp
+++ main.cpp
@@ -14,109 +14,61 @@
 using namespace std;
 
 void execute(const string& algorithm, long long int* array, long long int size_of_array){
-    if (algorithm == "insertionsort"){
-        auto start = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now();
+
+    if (algorithm == "insertionsort") {
         insertionSort(array, size_of_array);
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-        delete[] array;
-        cout << "Execution time: " << duration.count() << " microseconds" << endl;
-        ofstream archivo_salida; // Declara una instancia de ofstream para escritura
-        archivo_salida.open("../execution_time.txt", std::ios::app); // Abre el archivo en modo "append" para añadir datos
-
-        if (!archivo_salida) {
-            cerr << "No se pudo abrir el archivo." << endl;
-        }
-        else{
-            archivo_salida << "Execution time of insertionsort with array with "<< size_of_array << " elements: " << duration.count() << " microseconds"<< endl;
-        }
-        // Cierra el archivo
-        archivo_salida.close();
     }
-    else if (algorithm == "introsort"){
-        auto start = std::chrono::high_resolution_clock::now();
+    else if (algorithm == "introsort") {
         introsort(array, size_of_array);
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-        delete[] array;
-        cout << "Execution time: " << duration.count() << " microseconds" << endl;
-        ofstream archivo_salida; // Declara una instancia de ofstream para escritura
-        archivo_salida.open("../execution_time.txt", std::ios::app); // Abre el archivo en modo "append" para añadir datos
-
-        if (!archivo_salida) {
-            cerr << "No se pudo abrir el archivo." << endl;
-        }
-        else{
-            archivo_salida << "Execution time of introsort with array with "<< size_of_array << " elements: " << duration.count() << " microseconds"<< endl;
-        }
-        // Cierra el archivo
-        archivo_salida.close();
     }
-    else if (algorithm == "heapsort"){
-        auto start = std::chrono::high_resolution_clock::now();
+    else if (algorithm == "heapsort") {
         heapSort(array, size_of_array);
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-        delete[] array;
-        cout << "Execution time: " << duration.count() << " microseconds" << endl;
-        ofstream archivo_salida; // Declara una instancia de ofstream para escritura
-        archivo_salida.open("../execution_time.txt", std::ios::app); // Abre el archivo en modo "append" para añadir datos
-
-        if (!archivo_salida) {
-            cerr << "No se pudo abrir el archivo." << endl;
-        }
-        else{
-            archivo_salida << "Execution time of heapsort with array with "<< size_of_array << " elements: " << duration.count() << " microseconds"<< endl;
-        }
-        // Cierra el archivo
-        archivo_salida.close();
     }
-    auto *insertion = new long long int[size_of_array];
 
-    delete[] insertion;
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+    cout << "Execution time: " << duration.count() << " microseconds" << endl;
+
+    // Abre el archivo una sola vez fuera de las ramas condicionales
+    ofstream archivo_salida("../execution_time.txt", std::ios::app);
+
+    if (!archivo_salida) {
+        cerr << "No se pudo abrir el archivo." << endl;
+    }
+    else {
+        archivo_salida << "Execution time of " << algorithm << " with array of "<< size_of_array << " elements: " << duration.count() << " microseconds"<< endl;
+    }
+
+    // Cierra el archivo
+    archivo_salida.close();
+
+    // No necesitas asignar ni eliminar la matriz 'insertion' aquí
 }
 
 long long int* create_array(long long int size_of_array){
     srand(time(NULL));
     auto *array = new long long int[size_of_array];
     for (auto i = 0; i < size_of_array; i++) {
-        //array[i] = distribution(rand() % LLONG_MAX);
         array[i] = rand() % LLONG_MAX;
     }
     return array;
 }
 int main() {
-    //std::random_device rd;
-    //std::mt19937_64 generator(rd());
-    //std::uniform_int_distribution<long long int> distribution(1, LLONG_MAX);
+
     srand(time(NULL));
-    // 10
-    long long int size = 1000; // Size of array
-    auto array10 = create_array(size);
-
-    // 100
-    //size = 1000;
-    // 1000
-    //size = 1000;
-    // 10000
-    // 100000
-    // 1000000
-
-    // 10 elementos
-    //size = 100000; // Size of array
-    //auto *array = new long long int[size];
-
-
-    for (auto i = 0; i < size; i++) {
-        //array[i] = distribution(rand() % LLONG_MAX);
-        array10[i] = rand() % LLONG_MAX;
-    }
     string algorithm[] = {"insertionsort", "introsort", "heapsort"};
-    for (auto e : algorithm){
-        //long long int* temp = new long long int[size];
-        //temp = array10;
-        execute(e,array10,size);
+    long int times[] = {10, 100, 1000, 10000,100000,1000000};
+    long long int size = NULL; // Size of array
+    for (auto number : times){
+        size = number;
+        auto array = create_array(size);
+        for (auto algo : algorithm){
+            execute(algo,array,size);
+        }
     }
+
     //auto N = sizeof(array)/sizeof(array[0]);  Not necessary because obtain the same number of size
 
     return 0;
